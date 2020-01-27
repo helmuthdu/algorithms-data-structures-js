1fe8dbd89aa56e16028be60a074d93df64daa04d
Helmuth Saatkamp <helmuthdu@gmail.com>
2020-01-27 22:15:44 +0100

    added fibonacci example

---
+++ src/08-fib/fib.js
@@ -0,0 +1,41 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+const memoize = function(fn) {
+  const cache = {};
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn(args);
+    cache[args] = result;
+
+    return result;
+  };
+};
+
+const fibonacci = n => (n < 3 ? 1 : fib(n - 1) + fib(n - 2));
+
+const fib = memoize(fibonacci);
+
+// function fib(n) {
+//   const result = [0, 1];
+//
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+//     result.push(a + b);
+//   }
+//
+//   return result[n];
+// }
+
+module.exports = fib;
